efea90eb0c498a294fc2c92646b9769ddc6e1dc6
Paul Landes <landes@mailc.net>
2022-10-28 19:29:33 -0500

    fix missing resources issue

--- src/python/setup.py
+++ src/python/setup.py
@@ -5,11 +5,9 @@ su = SetupUtil(
     setup_path=Path(__file__).parent.absolute(),
     name="zensols.zotsite",
     package_names=['zensols', 'resources'],
-    # package_data={'': ['*.html', '*.js', '*.css', '*.map', '*.svg']},
-    package_data={'': ['*.conf', '*.json', '*.yml']},
+    package_data={'': ['*.conf', '*.yml', '*.html', '*.js', '*.css', '*.map', '*.svg', 'glyphicons*']},
     description='This project exports your local Zotero library to a usable HTML website.',
     user='plandes',
     project='zotsite',
     keywords=['tooling'],
-    # has_entry_points=False,
 ).setup()
