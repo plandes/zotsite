e0d110160ffde7f8d4a034cd6bb7283b8a91a0df
Paul Landes <landes@mailc.net>
2024-09-22 13:43:07 -0500

    doc

--- src/sh/zotsync.sh
+++ src/sh/zotsync.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
+# @meta {desc: 'zotsite export deployment', date: '2024-09-22'}
+
+
+# This script "deploys" an exported website using `zotsite` to a webserver
+# that also runs a samba server.
+#
+# Usage:
+# 1. Update the DOCROOT_URL and TARG variables below
+# 2. Remove the `--delete` flag to rsync during testing (careful)!
+# 3. Add back the `--delete` flag and optionally add to cron
+#
+# Paul Landes (9/22/2024)
+
 
 DOCROOT_URL=smb://YOUR/SERVER/PATH
-ZOT_DIR=$(mktemp -d)
 TARG=/Volumes/YOUR/LOCAL/MOUNT/POINT
+ZOT_DIR=$(mktemp -d)
 
 if [ ! -d "${TARG}" ] ; then
     echo "mount ${TARG} not found--mounting ${DOCROOT_URL}..."
@@ -18,6 +31,7 @@ if [ $? -ne 0 ] ; then
     echo "website generation error"
     exit 1
 fi
+# `--delete` is very destructive (careful)!
 rsync -rltpgoDuv --delete ${ZOT_DIR}/ ${TARG}
 
 if [ -d ${ZOT_DIR} ] ; then
