77580dd22eeadba37502c58ad58d3669bc2255e5
Paul Landes <landes@mailc.net>
2025-01-11 14:02:55 -0600

    new doc push

---
+++ demo/js/zotero-tree-data.js
@@ -0,0 +1,547 @@
+var tree =
+[
+  {
+    "text": "A Supervised Approach To The Interpretation Of Imperative To-Do Lists",
+    "item-id": "landesSupervisedApproachInterpretation2018",
+    "nodes": [
+      {
+        "text": "Comment: 9 pages; https://github.com/plandes/todo-task/",
+        "item-id": "n512",
+        "icon": "glyphicon glyphicon-text-background",
+        "item_title": "Comment: 9 pages; https://github.com/plandes/todo-task/",
+        "item_type": "note",
+        "item_note": "<div class=\"zotero-note znv1\"><p>Comment: 9 pages; https://github.com/plandes/todo-task/</p></div>",
+        "node_type": "note"
+      },
+      {
+        "text": "Landes - A Supervised Approach To The Interpretation Of Imp.pdf",
+        "item-id": "i516",
+        "icon": "glyphicon glyphicon-paperclip",
+        "item_title": "Landes - A Supervised Approach To The Interpretation Of Imp.pdf",
+        "item_type": "attachment",
+        "item_note": null,
+        "node_type": "item",
+        "metadata": [
+          [
+            "Title",
+            "Landes - A Supervised Approach To The Interpretation Of Imp.pdf"
+          ]
+        ],
+        "resource": "storage/i516.pdf"
+      },
+      {
+        "text": "arXiv.org Snapshot",
+        "item-id": "i514",
+        "icon": "glyphicon glyphicon-paperclip",
+        "item_title": "arXiv.org Snapshot",
+        "item_type": "attachment",
+        "item_note": null,
+        "node_type": "item",
+        "metadata": [
+          [
+            "Access Date",
+            "2018-06-25 01:33:25"
+          ],
+          [
+            "Title",
+            "arXiv.org Snapshot"
+          ],
+          [
+            "URL",
+            "https://arxiv.org/abs/1806.07999"
+          ]
+        ],
+        "resource": "storage/i514.html"
+      }
+    ],
+    "icon": "glyphicon glyphicon-file",
+    "item_title": "A Supervised Approach To The Interpretation Of Imperative To-Do Lists",
+    "item_type": "journalArticle",
+    "item_note": null,
+    "node_type": "item",
+    "metadata": [
+      [
+        "Abstract Note",
+        "To-do lists are a popular medium for personal information management. As to-do tasks are increasingly tracked in electronic form with mobile and desktop organizers, so does the potential for software support for the corresponding tasks by means of intelligent agents. While there has been work in the area of personal assistants for to-do tasks, no work has focused on classifying user intention and information extraction as we do. We show that our methods perform well across two corpora that span sub-domains, one of which we released."
+      ],
+      [
+        "Access Date",
+        "2018-06-25 01:33:23"
+      ],
+      [
+        "Citation Key",
+        "landesSupervisedApproachInterpretation2018"
+      ],
+      [
+        "Creators",
+        "Paul Landes, Barbara Di Eugenio"
+      ],
+      [
+        "Date",
+        "2018-06-20 2018-06-20"
+      ],
+      [
+        "Extra",
+        "arXiv: 1806.07999"
+      ],
+      [
+        "Library Catalog",
+        "arXiv.org"
+      ],
+      [
+        "Publication Title",
+        "arXiv:1806.07999 [cs]"
+      ],
+      [
+        "Title",
+        "A Supervised Approach To The Interpretation Of Imperative To-Do Lists"
+      ],
+      [
+        "URL",
+        "http://arxiv.org/abs/1806.07999"
+      ]
+    ],
+    "resource": "storage/i516.pdf",
+    "selectable": false
+  },
+  {
+    "text": "Exploring Difficulties in Parsing Imperatives and Questions",
+    "item-id": "haraExploringDifficultiesParsing",
+    "nodes": [
+      {
+        "text": "Hara et al. - Exploring Difficulties in Parsing Imperatives and .pdf",
+        "item-id": "i327",
+        "icon": "glyphicon glyphicon-paperclip",
+        "item_title": "Hara et al. - Exploring Difficulties in Parsing Imperatives and .pdf",
+        "item_type": "attachment",
+        "item_note": null,
+        "node_type": "item",
+        "metadata": [
+          [
+            "Title",
+            "Hara et al. - Exploring Difficulties in Parsing Imperatives and .pdf"
+          ]
+        ],
+        "resource": "storage/i327.pdf"
+      }
+    ],
+    "icon": "glyphicon glyphicon-file",
+    "item_title": "Exploring Difficulties in Parsing Imperatives and Questions",
+    "item_type": "journalArticle",
+    "item_note": null,
+    "node_type": "item",
+    "metadata": [
+      [
+        "Abstract Note",
+        "This paper analyzes the effect of the structural variation of sentences on parsing performance. We examine the performance of both shallow and deep parsers for two sentence constructions: imperatives and questions. We \ufb01rst prepare an annotated corpus for each of these sentence constructions by extracting sentences from a \ufb01ction domain that cover various types of imperatives and questions. The target parsers are then adapted to each of the obtained corpora as well as the existing query-focused corpus. Analysis of the experimental results reveals that the current mainstream parsing technologies and adaptation techniques cannot cope with different sentence constructions even with much in-domain data."
+      ],
+      [
+        "Citation Key",
+        "haraExploringDifficultiesParsing"
+      ],
+      [
+        "Creators",
+        "Tadayoshi Hara, Takuya Matsuzaki, Yusuke Miyao, Jun\u2019ichi Tsujii"
+      ],
+      [
+        "Language",
+        "en"
+      ],
+      [
+        "Library Catalog",
+        "Zotero"
+      ],
+      [
+        "Pages",
+        "9"
+      ],
+      [
+        "Title",
+        "Exploring Difficulties in Parsing Imperatives and Questions"
+      ]
+    ],
+    "resource": "storage/i327.pdf",
+    "selectable": false
+  },
+  {
+    "text": "On the generalized distance in statistics",
+    "item-id": "mahalanobisGeneralizedDistanceStatistics1936",
+    "nodes": [
+      {
+        "text": "Vol02_1936_1_Art05.pdf",
+        "item-id": "i59",
+        "icon": "glyphicon glyphicon-paperclip",
+        "item_title": "Vol02_1936_1_Art05.pdf",
+        "item_type": "attachment",
+        "item_note": null,
+        "node_type": "item",
+        "metadata": [
+          [
+            "Access Date",
+            "2018-03-06 05:50:38"
+          ],
+          [
+            "Title",
+            "Vol02_1936_1_Art05.pdf"
+          ],
+          [
+            "URL",
+            "http://insa.nic.in/writereaddata/UpLoadedFiles/PINSA/Vol02_1936_1_Art05.pdf"
+          ]
+        ],
+        "resource": "storage/i59.pdf"
+      }
+    ],
+    "icon": "glyphicon glyphicon-file",
+    "item_title": "On the generalized distance in statistics",
+    "item_type": "conferencePaper",
+    "item_note": null,
+    "node_type": "item",
+    "metadata": [
+      [
+        "Citation Key",
+        "mahalanobisGeneralizedDistanceStatistics1936"
+      ],
+      [
+        "Creators",
+        "Prasanta Chandra Mahalanobis"
+      ],
+      [
+        "Date",
+        "1936-00-00 1936"
+      ],
+      [
+        "Publisher",
+        "National Institute of Science of India"
+      ],
+      [
+        "Title",
+        "On the generalized distance in statistics"
+      ]
+    ],
+    "resource": "storage/i59.pdf",
+    "selectable": false
+  },
+  {
+    "text": "Semantics",
+    "item-id": "c98,i1297",
+    "nodes": [
+      {
+        "text": "Deep Semantic Role Labeling",
+        "item-id": "heDeepSemanticRole2017",
+        "nodes": [
+          {
+            "text": "Full Text PDF",
+            "item-id": "i1298",
+            "icon": "glyphicon glyphicon-paperclip",
+            "item_title": "Full Text PDF",
+            "item_type": "attachment",
+            "item_note": null,
+            "node_type": "item",
+            "metadata": [
+              [
+                "Access Date",
+                "2019-12-19 07:22:58"
+              ],
+              [
+                "Title",
+                "Full Text PDF"
+              ],
+              [
+                "URL",
+                "https://www.aclweb.org/anthology/P17-1044.pdf"
+              ]
+            ],
+            "resource": "storage/i1298.pdf"
+          }
+        ],
+        "icon": "glyphicon glyphicon-file",
+        "item_title": "Deep Semantic Role Labeling",
+        "item_type": "conferencePaper",
+        "item_note": null,
+        "node_type": "item",
+        "metadata": [
+          [
+            "Abstract Note",
+            "We introduce a new deep learning model for semantic role labeling (SRL) that significantly improves the state of the art, along with detailed analyses to reveal its strengths and limitations. We use a deep highway BiLSTM architecture with constrained decoding, while observing a number of recent best practices for initialization and regularization. Our 8-layer ensemble model achieves 83.2 F1 on theCoNLL 2005 test set and 83.4 F1 on CoNLL 2012, roughly a 10% relative error reduction over the previous state of the art. Extensive empirical analysis of these gains show that (1) deep models excel at recovering long-distance dependencies but can still make surprisingly obvious errors, and (2) that there is still room for syntactic parsers to improve these results."
+          ],
+          [
+            "Access Date",
+            "2019-12-19 07:22:57"
+          ],
+          [
+            "Citation Key",
+            "heDeepSemanticRole2017"
+          ],
+          [
+            "Conference Name",
+            "ACL 2017"
+          ],
+          [
+            "Creators",
+            "Luheng He, Kenton Lee, Mike Lewis, Luke Zettlemoyer"
+          ],
+          [
+            "DOI",
+            "10.18653/v1/P17-1044"
+          ],
+          [
+            "Date",
+            "2017-07-00 2017-07"
+          ],
+          [
+            "Library Catalog",
+            "ACLWeb"
+          ],
+          [
+            "Pages",
+            "473\u2013483"
+          ],
+          [
+            "Place",
+            "Vancouver, Canada"
+          ],
+          [
+            "Proceedings Title",
+            "Proceedings of the 55th Annual Meeting of the Association for Computational Linguistics (Volume 1: Long Papers)"
+          ],
+          [
+            "Publisher",
+            "Association for Computational Linguistics"
+          ],
+          [
+            "Short Title",
+            "Deep Semantic Role Labeling"
+          ],
+          [
+            "Title",
+            "Deep Semantic Role Labeling: What Works and What's Next"
+          ],
+          [
+            "URL",
+            "https://www.aclweb.org/anthology/P17-1044"
+          ]
+        ],
+        "resource": "storage/i1298.pdf",
+        "selectable": false
+      },
+      {
+        "text": "USFD at SemEval-2016 Task 6",
+        "item-id": "augensteinUSFDSemEval2016Task2016",
+        "nodes": [
+          {
+            "text": "Full Text PDF",
+            "item-id": "i760",
+            "icon": "glyphicon glyphicon-paperclip",
+            "item_title": "Full Text PDF",
+            "item_type": "attachment",
+            "item_note": null,
+            "node_type": "item",
+            "metadata": [
+              [
+                "Access Date",
+                "2018-09-09 12:54:41"
+              ],
+              [
+                "Title",
+                "Full Text PDF"
+              ],
+              [
+                "URL",
+                "http://www.aclweb.org/anthology/S16-1063.pdf"
+              ]
+            ],
+            "resource": "storage/i760.pdf"
+          }
+        ],
+        "icon": "glyphicon glyphicon-file",
+        "item_title": "USFD at SemEval-2016 Task 6",
+        "item_type": "conferencePaper",
+        "item_note": null,
+        "node_type": "item",
+        "metadata": [
+          [
+            "Access Date",
+            "2018-09-09 12:54:41"
+          ],
+          [
+            "Citation Key",
+            "augensteinUSFDSemEval2016Task2016"
+          ],
+          [
+            "Creators",
+            "Isabelle Augenstein, Andreas Vlachos, Kalina Bontcheva"
+          ],
+          [
+            "Date",
+            "2016-06-00 June 2016"
+          ],
+          [
+            "Library Catalog",
+            "ACLWeb"
+          ],
+          [
+            "Pages",
+            "389\u2013393"
+          ],
+          [
+            "Place",
+            "San Diego, California"
+          ],
+          [
+            "Proceedings Title",
+            "Proceedings of the 10th International Workshop on Semantic Evaluation (SemEval-2016)"
+          ],
+          [
+            "Publisher",
+            "Association for Computational Linguistics"
+          ],
+          [
+            "Short Title",
+            "USFD at SemEval-2016 Task 6"
+          ],
+          [
+            "Title",
+            "USFD at SemEval-2016 Task 6: Any-Target Stance Detection on Twitter with Autoencoders"
+          ],
+          [
+            "URL",
+            "http://www.aclweb.org/anthology/S16-1063"
+          ]
+        ],
+        "resource": "storage/i760.pdf",
+        "selectable": false
+      }
+    ],
+    "item_title": "Semantics",
+    "item_type": null,
+    "item_note": null,
+    "node_type": "collection",
+    "selectable": false
+  },
+  {
+    "text": "Weighted kappa",
+    "item-id": "cohenWeightedKappaNominal1968",
+    "nodes": [
+      {
+        "text": "cohens-kappa.pdf",
+        "item-id": "i20",
+        "icon": "glyphicon glyphicon-paperclip",
+        "item_title": "cohens-kappa.pdf",
+        "item_type": "attachment",
+        "item_note": null,
+        "node_type": "item",
+        "metadata": [
+          [
+            "Title",
+            "cohens-kappa.pdf"
+          ]
+        ],
+        "resource": "storage/i20.pdf"
+      }
+    ],
+    "icon": "glyphicon glyphicon-file",
+    "item_title": "Weighted kappa",
+    "item_type": "journalArticle",
+    "item_note": null,
+    "node_type": "item",
+    "metadata": [
+      [
+        "Citation Key",
+        "cohenWeightedKappaNominal1968"
+      ],
+      [
+        "Creators",
+        "Jacob Cohen"
+      ],
+      [
+        "Date",
+        "1968-00-00 1968"
+      ],
+      [
+        "Issue",
+        "4"
+      ],
+      [
+        "Library Catalog",
+        "Google Scholar"
+      ],
+      [
+        "Pages",
+        "213"
+      ],
+      [
+        "Publication Title",
+        "Psychological bulletin"
+      ],
+      [
+        "Short Title",
+        "Weighted kappa"
+      ],
+      [
+        "Title",
+        "Weighted kappa: Nominal scale agreement provision for scaled disagreement or partial credit."
+      ],
+      [
+        "Volume",
+        "70"
+      ]
+    ],
+    "resource": "storage/i20.pdf",
+    "selectable": false
+  },
+  {
+    "text": "python - NLTK Thinks that Imperatives are Nouns - Stack Overflow",
+    "item-id": "PythonNLTKThinks",
+    "nodes": [
+      {
+        "text": "Snapshot",
+        "item-id": "i103",
+        "icon": "glyphicon glyphicon-paperclip",
+        "item_title": "Snapshot",
+        "item_type": "attachment",
+        "item_note": null,
+        "node_type": "item",
+        "metadata": [
+          [
+            "Access Date",
+            "2018-03-11 16:59:49"
+          ],
+          [
+            "Title",
+            "Snapshot"
+          ],
+          [
+            "URL",
+            "https://stackoverflow.com/questions/9406093/nltk-thinks-that-imperatives-are-nouns"
+          ]
+        ],
+        "resource": "storage/i103.html"
+      }
+    ],
+    "icon": "glyphicon glyphicon-bookmark",
+    "item_title": "python - NLTK Thinks that Imperatives are Nouns - Stack Overflow",
+    "item_type": "webpage",
+    "item_note": null,
+    "node_type": "item",
+    "metadata": [
+      [
+        "Access Date",
+        "2018-03-11 16:59:46"
+      ],
+      [
+        "Citation Key",
+        "PythonNLTKThinks"
+      ],
+      [
+        "Title",
+        "python - NLTK Thinks that Imperatives are Nouns - Stack Overflow"
+      ],
+      [
+        "URL",
+        "https://stackoverflow.com/questions/9406093/nltk-thinks-that-imperatives-are-nouns"
+      ]
+    ],
+    "selectable": false
+  }
+]
